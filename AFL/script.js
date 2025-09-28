bcc91f56ebfeddb476e7daa2cc4a717fa6de2536
arkhanarkhan82 <arkhanarkhan82@gmail.com>
2025-09-28 12:56:10 +0600

    Update script.js

--- AFL/script.js
+++ AFL/script.js
@@ -49,8 +49,7 @@ fetch(apiURL)
             <td>${event.tournament || "-"}</td>
             <td>${event.match || "-"}</td>
             <td>
-              <a class="watch-btn" target="_blank"
-                 href="/StreamPage/?id=${event.unix_timestamp}_${idx}">
+              <a class="watch-btn" href="/StreamPage/?id=${event.unix_timestamp}_${idx}">
                  Watch
               </a>
             </td>
@@ -74,5 +73,6 @@ fetch(apiURL)
     loadingDiv.innerHTML = `<p style="color:red;">⚠ Error loading matches</p>`;
     console.error(err);
   });
+
 
 
