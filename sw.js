2f59d4fd83409690a32f0bc2fceca8d8957e7b75
arkhanarkhan82 <arkhanarkhan82@gmail.com>
2025-10-20 21:02:08 +0600

    Update sw.js

--- sw.js
+++ sw.js
@@ -5,10 +5,12 @@ self.addEventListener('fetch', (event) => {
 
 });
 
-
+// Monetag Verification.
 self.options = {
     "domain": "5gvci.com",
     "zoneId": 10068451
 }
 self.lary = ""
 importScripts('https://5gvci.com/act/files/service-worker.min.js?r=sw')
+
+// Monetag Verification END.
