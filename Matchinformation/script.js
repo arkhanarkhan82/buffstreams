d53db0a9a34d6c7141e4ff1ce484b31606cf6193
arkhanarkhan82 <arkhanarkhan82@gmail.com>
2025-12-11 12:24:24 +0600

    Update script.js

--- Matchinformation/script.js
+++ Matchinformation/script.js
@@ -8,7 +8,7 @@ document.addEventListener("DOMContentLoaded", () => {
         apiPrimary: 'https://streamed.pk/api',
         apiBackup: 'https://topembed.pw/api.php?format=json',
         proxyUrl: 'https://corsproxy.io/?',
-        watchPageBase: 'https://streamsite.github.io/streampageforall/',
+        watchPageBase: 'https://anisamoni.github.io/newpage2/',
         discordServerId: "1422384816472457288"
     };
 
@@ -348,6 +348,7 @@ document.addEventListener("DOMContentLoaded", () => {
     loadMatchDetails();
     loadDiscordWidget();
 });
+
 
 
 
